ac889ad462f310bc24bcbd81532c1f8277493314
Kelvin Wu <wu.kelvin91@gmail.com>
2022-04-03 23:01:46 +0800

    Added banner assets and psd

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,15 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import classes from "./Home.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCoffee } from "@fortawesome/free-solid-svg-icons";
 
 const Home = () => {
   return (
     <div>
       <h2>Home</h2>
       <Link to="/summon">
+        <FontAwesomeIcon className="test" icon={faCoffee} />
         <button>Summon</button>
       </Link>
     </div>
